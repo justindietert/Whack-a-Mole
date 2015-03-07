d4924eb7adf0a057dde7cbf7829dc33c236d9d2e
Justin Dietert <justin.dietert@gmail.com>
2015-03-06 22:18:27 -0600

    added button animations and blood splat on click

--- public/js/whack.js
+++ public/js/whack.js
@@ -10,7 +10,19 @@
         $('#start').on('click', startGame);
 
         function startGame(){
-            $('#start').off('click', startGame).addClass('whack').html('WHACK!');
+            // $('#prompt').removeClass('lime');
+            $('#timer').html('<span id="seconds" >__</span>&nbsp;second<span id="hide-one">s</span> left');
+            $('#start').off('click', startGame).addClass('whack').html('Whack!');
+            $('#prompt').addClass('lime').html('Points to win: 30');
+            setTimeout(function(){
+                $('#prompt').removeClass('lime');
+            }, 1500);
+
+            // $('#prompt').animate({
+            //     color: '#222222'
+            // }, 3000);
+
+            $('#points').html('');
             timer = 30;
             score = 0;
             animateBoard();
@@ -22,20 +34,19 @@
             var moles = $('.mole');
             var randomMole = moles[random];
 
-            console.log(random);
+            // console.log(random);
 
             $(randomMole).show('slide', {direction: 'down'}, 400);
 
             $(randomMole).off().one('mousedown', function(){
-                // $(this).effect('highlight', {color: 'red'}, 25);
+                // $(this).effect('bounce');
                 score++;
                 $('#points').html(score);
-                // $(this).off('mousedown');
             });
  
             setTimeout(function(){
                 $(randomMole).hide('slide', {direction: 'down'}, 200);
-            }, 1000);      
+            }, 800);      
         }
 
         function animateRandomMoleTwo() {
@@ -43,33 +54,20 @@
             var moles = $('.mole');
             var randomMole = moles[random];
 
-            console.log(random);
+            // console.log(random);
 
             $(randomMole).show('slide', {direction: 'down'}, 400).delay(1000);
 
             $(randomMole).off().one('mousedown', function(){
-                // $(this).effect('highlight', {color: 'red'}, 25);
+                // $(this).effect('bounce');
                 score++;
                 $('#points').html(score);
-                // $(this).off('mousedown');
             });
  
             setTimeout(function(){
                 $(randomMole).hide('slide', {direction: 'down'}, 200);
-            }, 1000);      
+            }, 800);      
         }
-
-        // function animateRandomMoleTwo() {
-        //     var random = Math.floor(Math.random() * 9);
-        //     var moles = $('.mole');
-        //     var randomMole = moles[random];
-
-        //     $(randomMole).show('slide', {direction: 'down'}, 800).delay(500);
-
-        //     setTimeout(function(){
-        //         $(randomMole).hide('slide', {direction: 'down'}, 200);
-        //     }, 1000);
-        // }
 
         // function animateRandomMoleThree() {
         //     var random = Math.floor(Math.random() * 9);
@@ -85,23 +83,28 @@
 
         function animateBoard() {
             var intervalId = setInterval(function(){
-                if (timer >= 0) {
+                if (timer > 0) {
 
                     animateRandomMoleOne();
                     animateRandomMoleTwo();
                     // animateRandomMoleThree();
 
                     $('#seconds').html(timer);
+                    if (timer == 1) {
+                        $('#hide-one').addClass('hide-one');
+                    } else {
+                        $('#hide-one').removeClass('hide-one');
+                    }
 
                     timer--;
                 } else {
                     clearInterval(intervalId);
-                    alert('Game Over');
+                    $('#timer').html('Game Over<span class="emph">!</span>');
                     $('#start').on('click', startGame);
-                    $('#seconds').html('__');
-                    $('#points').html('');
-                    score = 0;
-                    $('#start').removeClass('whack').html('Start');
+                    $('#start').removeClass('whack').html('Start').addClass('restart');
+                    $('#prompt').html('Click start to play again.');
+
+                    // need to add high score feature
                 }
             }, 1000);
         }
